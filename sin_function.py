f17cbb7b84515886b6f6ba59142f189926626a36
kpspyolo024 <kshitizpratapsinghparihar@gmail.com>
2026-03-26 11:32:52 +0530

    Create sin_function.py
    finds value of sin (at error 10^-5

---
+++ sin_function.py
@@ -0,0 +1,8 @@
+print("question: design an algorithm to evaluate the function sin(x) as defined as a infinite series expansion.")
+x=float(input("enter the value of x:"))
+j=x
+tsin=x
+term=x
+i=1
+x2=x*x
+err=10**-5
